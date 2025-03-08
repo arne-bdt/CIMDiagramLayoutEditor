d7b352e9073560960b544bacc478e994100c53aa
arne-bdt <arne.bernhardt@gmail.com>
2025-03-08 20:37:29 +0100

    fixed wrong location of index.html

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
       '@': resolve('./src')
     }
   },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
   server: {
     host: true,
     port: 3000
